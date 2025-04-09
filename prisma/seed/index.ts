1408bba36566957ee8ce89371f8729cf9cfdd59d
Vladislav KUNITSYN <vladislav.kunitsyn@gmail.com>
2025-04-09 16:35:28 +0200

    ajoustement de seed

--- prisma/seed/index.ts
+++ prisma/seed/index.ts
@@ -10,6 +10,185 @@ function randomInt(min: number, max: number): number {
 }
 
 
+
+async function seedProfil(number = 5) {
+
+    const profiles = [];
+
+    while (number) {
+        // Creation du faker
+        const profile = {
+            picture: fakerFR.image.urlPicsumPhotos(),
+            street: fakerFR.location.street(),
+            zip_code: fakerFR.location.zipCode(),
+            city: fakerFR.location.city(),
+            phone_number: fakerFR.phone.number(),
+        }
+        // SEED
+        const newProfil = await prisma.profil.upsert({
+            where: {
+                phone_number: profile.phone_number
+            },
+            update: {},
+            create: {
+                ...profile
+            }
+        })
+        // ajoute dans le tableau des teams
+        profiles.push(newProfil);
+
+        // décrémenter number
+        number--; // number = number - 1;
+    }
+
+    return profiles;
+
+}
+
+async function seedLevel() {
+    const grades = ['admin', 'pas_admin'];
+    const levels = [];
+
+    for (const grade of grades) {
+        const newLevel = await prisma.level.upsert({
+            where: { grade },
+            update: {},
+            create: { grade }
+        });
+
+        levels.push(newLevel);
+    }
+
+    return levels;
+}
+
+async function seedEnterprise (number = 66) {
+
+    const enterprises = [];
+
+    while (number) {
+        // Creation du faker
+        const enterprise = {
+            name: faker.company.name(),
+            employees: faker.number.int({ min: 1, max: 100 }),
+            description: faker.lorem.sentence()
+        }
+        // SEED
+        const newEnterprise = await prisma.enterprise.create({
+            data: enterprise
+
+        })
+        // ajoute dans le tableau des teams
+        enterprises.push(newEnterprise);
+
+        // décrémenter number
+        number--; // number = number - 1;
+    }
+
+    return enterprises;
+
+}
+
+async function seedQuestion (number = 42) {
+
+    const questions = [];
+
+    while (number) {
+        // Creation du faker
+        const question = {
+            question: faker.lorem.sentence(),
+        }
+        // SEED
+        const newQuestion = await prisma.question.create({
+            data:question
+        })
+        // ajoute dans le tableau des teams
+        questions.push(newQuestion);
+
+        // décrémenter number
+        number--; // number = number - 1;
+    }
+
+    return questions;
+
+}
+
+async function seedAnswer (number = 42) {
+
+    const answers = [];
+
+    while (number) {
+        // Creation du faker
+        const answer = {
+            answer: faker.lorem.sentence(),
+        }
+        // SEED
+        const newAnswer = await prisma.answer.create({
+            data:answer
+        })
+        // ajoute dans le tableau des teams
+        answers.push(newAnswer);
+
+        // décrémenter number
+        number--; // number = number - 1;
+    }
+
+    return answers;
+
+}
+
+async function seedSurvey (number = 15) {
+
+    const surveys = [];
+
+    while (number) {
+        // Creation du faker
+        const survey = {
+            message: faker.lorem.text(),
+            question: faker.lorem.sentence(),
+            answer: faker.lorem.sentence(),
+        }
+        // SEED
+        const newSurvey = await prisma.survey.create({
+            data:survey
+        })
+        // ajoute dans le tableau des teams
+        surveys.push(newSurvey);
+
+        // décrémenter number
+        number--; // number = number - 1;
+    }
+
+    return surveys;
+
+}
+
+async function seedLanguage (number = 19) {
+    const languages = [];
+
+    while (number) {
+        // Creation du faker
+        const language = {
+            name: fakerFR.location.language().name
+
+        }
+        // SEED
+        const newLanguage = await prisma.language.create({
+            data:language
+        })
+        // ajoute dans le tableau des teams
+        languages.push(newLanguage);
+
+        // décrémenter number
+        number--; // number = number - 1;
+    }
+
+    return languages;
+
+}
+
+/*
+
 async function seedUser(number = 50) {
     const users = [];
     while (number) {
@@ -58,7 +237,7 @@ async function seedCurriculumVitae(number = 3) {
             phone_number: fakerFR.phone.number(),
         }
         // SEED
-        const newUser = await prisma.curriculum_vitae.upsert ({
+        const newCurriculumVitae = await prisma.curriculum_vitae.upsert ({
             where: {
                 phone_number: curriculumVitae.phone_number
             },
@@ -68,7 +247,7 @@ async function seedCurriculumVitae(number = 3) {
             }
         })
         // ajoute dans le tableau des teams
-        curriculumVitae.push(newUser);
+        curriculumVitaes.push(newCurriculumVitae);
 
         // décrémenter number
         number--; // number = number - 1;
@@ -78,60 +257,25 @@ async function seedCurriculumVitae(number = 3) {
 
 }
 
-async function seedProfil(number = 5) {
-
-    const profiles = [];
-
-    while (number) {
-        // Creation du faker
-        const profile = {
-            picture: fakerFR.image.urlPicsumPhotos(),
-            street: fakerFR.location.street(),
-            zip_code: fakerFR.location.zipCode(),
-            city: fakerFR.location.city(),
-            phone_number: fakerFR.phone.number(),
-        }
-        // SEED
-        const newUser = await prisma.profil.upsert({
-            where: {
-                phone_number: profile.phone_number
-            },
-            update: {},
-            create: {
-                ...profile
-            }
-        })
-        // ajoute dans le tableau des teams
-        profiles.push(newUser);
-
-        // décrémenter number
-        number--; // number = number - 1;
-    }
-
-    return profiles;
-
-}
-
-async function seedApplication (number = 15) {
+async function seedApplication (number = 15, users) {
     const applications = [];
 
     while (number) {
         // Creation du faker
         const application = {
-            received_return: fakerFR.lorem.text()
+            received_return: faker.number.int({ min: 0, max: 3 }),
+            number_of_reminder: faker.number.int({ min: 0, max: 5 }),
+            id: [randomInt(0, users.length)]
         }
         // SEED
-        const newUser = await prisma.application.upsert({
-            where: {
-                id: application.userId
-            },
-            update: {},
-            create: {
-                ...application
+        const newApplication = await prisma.application.create({
+
+            data: {
+
             }
         })
         // ajoute dans le tableau des teams
-        applications.push(newUser);
+        applications.push(newApplication);
 
         // décrémenter number
         number--; // number = number - 1;
@@ -139,3 +283,63 @@ async function seedApplication (number = 15) {
 
     return applications;
 }
+
+async function seedOffer (number = 15, users) {
+    const offers = [];
+
+    while (number) {
+        // Creation du faker
+        const offer = {
+            title: faker.commerce.productName(),
+            description: faker.commerce.productDescription(),
+            id: [randomInt(0, users.length)]
+        }
+        // SEED
+        const newOffer = await prisma.offer.upsert({
+            where: {
+                id: offer.id,
+            update: {},
+            create: {
+                ...offer
+            }
+        })
+        // ajoute dans le tableau des teams
+        offers.push(newOffer);
+
+        // décrémenter number
+        number--; // number = number - 1;
+    }
+
+    return offers;
+
+}
+
+async function seedLevelGrade (number = 2) {
+
+    const levelGrades = [];
+
+    while (number) {
+        // Creation du faker
+        const levelGrade = {
+            admin: number
+        }
+        // SEED
+        const newLevelGrade = await prisma.level_grade.upsert({
+            where: {
+                name: levelGrade.name
+            },
+            update: {},
+            create: {
+                ...levelGrade
+            }
+        })
+        // ajoute dans le tableau des teams
+        levelGrades.push(newUser);
+
+        // décrémenter number
+        number--; // number = number - 1;
+    }
+
+    return levelGrades;
+
+}*/

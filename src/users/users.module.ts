fb475654e78673cf0aa566f0270964c1cdf0d1fd
Vladislav KUNITSYN <vladislav.kunitsyn@gmail.com>
2025-04-16 14:57:30 +0200

    injection prisma

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,10 @@
 
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
+import {AppModule} from "../app.module";
 
 @Module({
+  imports: [AppModule],
   providers: [UsersService],
   exports: [UsersService],
 })
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,12 +1,61 @@
-
 import { Injectable } from '@nestjs/common';
+import {PrismaService} from "../../prisma/prisma.service";
+import { User, Prisma } from '@prisma/client';
 
-// This should be a real class/interface representing a user entity
-export type User = any;
 
 @Injectable()
 export class UsersService {
-    private readonly users = [
+    constructor(private prisma: PrismaService) {}
+
+    async user(
+        userWhereUniqueInput: Prisma.UserWhereUniqueInput,
+    ): Promise<User | null> {
+        return this.prisma.user.findUnique({
+            where: userWhereUniqueInput,
+        });
+    }
+
+    async users(params: {
+        skip?: number;
+        take?: number;
+        cursor?: Prisma.UserWhereUniqueInput;
+        where?: Prisma.UserWhereInput;
+        orderBy?: Prisma.UserOrderByWithRelationInput;
+    }): Promise<User[]> {
+        const { skip, take, cursor, where, orderBy } = params;
+        return this.prisma.user.findMany({
+            skip,
+            take,
+            cursor,
+            where,
+            orderBy,
+        });
+    }
+
+    async createUser(data: Prisma.UserCreateInput): Promise<User> {
+        return this.prisma.user.create({
+            data,
+        });
+    }
+
+    async updateUser(params: {
+        where: Prisma.UserWhereUniqueInput;
+        data: Prisma.UserUpdateInput;
+    }): Promise<User> {
+        const { where, data } = params;
+        return this.prisma.user.update({
+            data,
+            where,
+        });
+    }
+
+    async deleteUser(where: Prisma.UserWhereUniqueInput): Promise<User> {
+        return this.prisma.user.delete({
+            where,
+        });
+    }
+
+    /*private readonly users = [
         {
             userId: 1,
             email: 'Primerose.Dupuy@yahoo.fr',
@@ -21,5 +70,5 @@ export class UsersService {
 
     async findOne(email: string): Promise<User | undefined> {
         return this.users.find(user => user.email === email);
-    }
+    }*/
 }

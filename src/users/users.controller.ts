e4911b8b59aadd44251adb491890ba8fe3d62623
Vladislav KUNITSYN <vladislav.kunitsyn@gmail.com>
2025-04-17 17:49:38 +0200

    ajout de argon2 pour hacher le mdp création des dto

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,6 @@
-import {Controller, Get} from '@nestjs/common';
+import {Body, Controller, Get, Post} from '@nestjs/common';
 import { UsersService } from './users.service';
+import { Public } from 'src/auth/public.decorator';
 
 @Controller('users')
 export class UsersController {
@@ -9,4 +10,16 @@ export class UsersController {
   findAll() {
     return this.usersService.findAll();
   }
+
+/*  @Public()
+  @Post('signup')
+  async createUser(@Body() body: {
+    email: string;
+    password: string;
+    firstname: string;
+    lastname: string;
+    profilId: number;
+    levelId: number;}) {
+    return this.usersService.createUser(body);
+  }*/ //transfer dans auth
 }
